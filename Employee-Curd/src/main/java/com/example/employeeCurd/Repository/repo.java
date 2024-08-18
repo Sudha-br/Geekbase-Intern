ead5ae21e59dfc2159ef6ca2754d98ec30b83d32
Sudha-br <rphsudha@gmail.com>
2024-08-18 14:59:00 +0530

    Delete Employee-Curd/src directory

--- Employee-Curd/src/main/java/com/example/employeeCurd/Repository/repo.java
+++
@@ -1,9 +0,0 @@
-package com.example.employeeCurd.Repository;
-
-import com.example.employeeCurd.entity.employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface repo extends JpaRepository<employee,Integer> {
-}
